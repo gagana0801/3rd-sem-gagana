945811e989cea7164314aa23f88ed576803e7a98
gagana0801 <gagana082007@gmail.com>
2025-10-17 15:22:18 +0530

    Add files via upload

---
+++ DAY7/GitProfile/src/App.jsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { useState } from "react";
+
+const App = () => {
+  const [user, setUser] = useState({});
+
+  const searchGithub = async (username) => {
+    const { data } = await axios.get(
+      "https://api.github.com/users/musa2468"
+    );
+    console.log(data);
+
+    setUser(data);
+    console.log(user);
+  };
+
+  searchGithub();
+
+return (
+
+<>
+
+<h1>Github Search</h1> <img src={user.avatar_url} alt=""width={"10%"} /> <h2>{user.name}</h2> <p>{user.bio}</p>
+
+</>
+
+);
+};
+
+export default App;
